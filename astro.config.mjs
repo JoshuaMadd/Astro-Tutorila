1167307dd7516f957f89756537b01a6374efcdce
JoshuaMadd <joshua.maddelein@student.vives.be>
2024-02-17 00:24:57 +0100

    U6 - islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://joshuamadd.github.io',
-    base: '/Astro-Tutorila'
+  site: 'https://joshuamadd.github.io',
+  base: '/Astro-Tutorila',
+  integrations: [preact()]
 });
